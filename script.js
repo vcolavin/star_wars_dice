a62e0cfd6910b1bc5d5fa5017c7a98d4c7441756
Vincent Colavin <vcolavin@users.noreply.github.com>
2018-01-18 20:22:22 -0800

    WIP dice object

--- script.js
+++ script.js
@@ -17,3 +17,16 @@ function onPlusClick(e) {
   let currentValue = parseInt($(e.target).siblings('.counter').html())
   $(e.target).siblings('.counter').html(currentValue + 1)
 }
+
+function select(anArray) {
+  return anArray[Math.floor(Math.random()*anArray.length)]
+}
+
+const dice = {
+  green: [],
+  yellow: [[], ["success"], ["success"], ["success", "success"], ["advantage"], ["advantage"], ["success", "advantage"], ["advantage", "advantage"]],
+  blue: [[], [], ["success"], ["success", "advantage"], ["advantage", "advantage"], ["advantage"]],
+  purple: [],
+  red: [],
+  black: [[], [], ["failure"], ["failure"], ["setback"], ["setback"]]
+}
