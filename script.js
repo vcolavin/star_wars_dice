a80009800a406e78725a1f338efe5fa3c08c0c27
Vincent Colavin <vcolavin@users.noreply.github.com>
2018-01-18 21:34:40 -0800

    ES6

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-$(function() {
+$(() => {
   $(".counter-container .minus").click(onMinusClick)
   $(".counter-container .plus").click(onPlusClick)
   $(".dice-roll-button").click(rollEm)
 })
 
-function rollEm(e) {
-  e.preventDefault()
-  // getSums()
-}
-
-function onMinusClick(e) {
+onMinusClick = e => {
   e.preventDefault()
   let currentValue = parseInt($(e.target).siblings('.counter').html())
   if (currentValue > 0) {
@@ -17,15 +12,20 @@ function onMinusClick(e) {
   }
 }
 
-function onPlusClick(e) {
+onPlusClick = e => {
   e.preventDefault()
   let currentValue = parseInt($(e.target).siblings('.counter').html())
   $(e.target).siblings('.counter').html(currentValue + 1)
 }
 
-function select(anArray) {
-  return anArray[Math.floor(Math.random()*anArray.length)]
+ rollEm e => {
+  e.preventDefault()
+  console.log(diceCounts())
 }
+
+diceCounts = () => $('.dice-list-item').map((element) => $(element).children('.counter').html())
+
+select = anArray => anArray[Math.floor(Math.random()*anArray.length)]
 
 const diceMap = {
   1: "success",
