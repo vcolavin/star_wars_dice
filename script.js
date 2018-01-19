b7990e5489092dfa2f27ffc368e799ac01d32c0c
Vincent Colavin <vcolavin@users.noreply.github.com>
2018-01-18 20:07:32 -0800

    add counters (I think)

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('hey')
+$(function() {
+  $(".counter-container .minus").click(onMinusClick)
+  $(".counter-container .plus").click(onPlusClick)
+})
+
+
+function onMinusClick(e) {
+  e.preventDefault()
+  let currentValue = parseInt($(e.target).siblings('.counter').val())
+  if (currentValue > 1) {
+    $(e.target).siblings('.counter').val(currentValue - 1)
+  }
+}
+
+function onPlusClick(e) {
+  e.preventDefault()
+  let currentValue = parseInt($(e.target).siblings('.counter').val())
+  $(e.target).siblings('.counter').val(currentValue + 1)
+}
